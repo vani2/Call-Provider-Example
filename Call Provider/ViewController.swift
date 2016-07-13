2ef2099bf66684337cb71da81b379cb7f5f9cc30
Ivan Vavilov <iv@redmadrobot.com>
2016-07-13 10:08:33 +0300

    Initial Commit

---
+++ Call Provider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Call Provider
+//
+//  Created by Ivan Vavilov on 7/13/16.
+//  Copyright © 2016 Ivan Vavilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
